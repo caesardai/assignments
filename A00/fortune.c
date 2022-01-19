84df7e21b4d36a2828479f7799835b944bacef62
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-01-19 03:55:57 +0000

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/*********************************************
+ * fortune.c
+ * Author: Caesar Dai
+ * Date 1/18/2022
+ * Print a fortune of my choosing
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Carpe diem, seize the day\n.");
+	return 0;
+}
