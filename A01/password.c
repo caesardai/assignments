66c101bb5e9ef30d9fbec082e57bdb2e066ff2c6
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-01-28 00:59:12 +0000

    saving first two programs

--- A01/password.c
+++ A01/password.c
@@ -1,3 +1,28 @@
-int main() {
-  return 0;
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char main() {
+	char str[20];
+	int str_length;
+
+	printf("Enter a password: ");
+	scanf("%s", str);
+
+	str_length = strlen(str);
+
+	for (int i = 0; i < str_length; i ++) {
+		if (str[i] == 'e') {
+			str[i] = '3';
+		}
+		if (str[i] == 'l') {
+                        str[i] = '1';
+                }
+		if (str[i] == 'a') {
+                        str[i] = '@';
+                }
+	}
+
+	printf("%s\n", str);
+	return 0;
 }
