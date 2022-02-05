69d40ee26e3742192f17f68548a6c613573fa33b
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-02-05 03:21:42 +0000

    submitting repeat.c

--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
+  char *word;
+  int *count;
+  
+  // allocate memory
+  word = malloc(sizeof(char) * 32); // allocate an array of 32 chars on the heap
+                                    // after 'word' stores the base address of
+                                    //an array of 32 contiguous char storage locations in heap memory
+  if (word)
+  {
+    printf("malloc is successful for allocating space for word\n");
+  }
+  else
+  {
+    printf("malloc failed for allocating space for word\n");
+  }
+
+  count = malloc(sizeof(int)); //allocate heap memory for storing an int
+  if (count)
+  {
+    printf("malloc is successful for allocating space for count\n");
+  }
+  else
+  {
+    printf("malloc failed for allocating space for count\n");
+  }
+
+  printf("Enter a word: ");
+  scanf("%s", word);
+  printf("Enter a count: ");
+  scanf("%d", count);
+
+  for (int i = 0; i < *count; i++)
+  {
+    printf("%s", word);
+  }
+
+  // freeing memory
+  free(word);
+  word = NULL;
+
+  free(count);
+  count = NULL;
   return 0;
 }
