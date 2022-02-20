c3d923d86cec5c1bf0b1210a6e2aab7bcf388da3
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-02-19 20:11:33 -0500

    complete raeding ppm

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -6,8 +6,65 @@
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
-struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+struct ppm_pixel** read_ppm(const char* filename, int* w, int* h) {
+  
+  FILE* infile = NULL;
+  unsigned char r, g, b;
+  //char filetype[10];
+  char buffer[1024]; // allocate 1024 for potential use
+  struct ppm_pixel** pixel = NULL;
 
-  return NULL;
+  infile = fopen(filename, "r");
+  if (infile == NULL) { // error message if can't open file
+    printf("Cannot open file. Exiting.\n");
+    return NULL;
+  }
+
+  // check whether it's ASCII
+  fgets(buffer, 1024, infile);
+  // if (strcmp(buffer, 'P3') != 0) {
+  //   printf("Incorrect file type. Need to be in ASCII");
+  //   return NULL;
+  // }
+
+  // read in comment or skip any other kinds of blank lines
+  while (buffer[0] == '#' || buffer[0] == ' ' || 
+    buffer[0] == '\n' || buffer[0] == '\r') {
+    fgets(buffer, 1024, infile);
+  }
+
+  // read in width and height
+  fgets(buffer, 1024, infile);
+  sscanf(buffer, "%d %d", w, h);
+  
+  // assume the maximum color value will always be correct
+  fgets(buffer, 1024, infile);
+
+  // allocate memory for 2D array of arrays
+  pixel = malloc(sizeof(struct ppm_pixel*) * *h);
+  if (pixel == NULL) {
+    printf("allocate new memory failed. Exit.\n");
+    return NULL;
+  }
+
+  for (int i = 0; i < *h; i++) {
+    pixel[i] = malloc(sizeof(struct ppm_pixel) * *w);
+    if (pixel[i] == NULL) {
+      printf("allocate new memory failed. Exit.\n");
+      return NULL;
+    }
+  }
+
+  // fill the 2D array
+  for (int i = 0; i < *h; i++) {
+    for (int j = 0; j < *w; j++) {
+      fscanf(infile, " %hhu %hhu %hhu", &r, &g, &b);
+      pixel[i][j].red = r;
+      pixel[i][j].green = g;
+      pixel[i][j].blue = b;
+    }
+  } 
+
+  fclose(infile);
+  return pixel;
 }
-
--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
 int main() {
+  int w;
+  int h;
+  char* input = "feep-ascii.ppm"; 
+  struct ppm_pixel** graph_matrix = NULL;
 
   // todo: call read_ppm
+  graph_matrix = read_ppm(input, &w, &h);
+  
+  printf("Testing file %s: %d %d\n", input, w, h);
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      printf("(%d,%d,%d) ", 
+        graph_matrix[i][j].red, graph_matrix[i][j].green, graph_matrix[i][j].blue);
+    }
+    printf("\n");
+  }
 
+  for (int i = 0; i < w; i++) {
+    free(graph_matrix[i]);
+    graph_matrix[i] = NULL;
+  }
+  free(graph_matrix);
+  graph_matrix = NULL;
   return 0;
 }
-
