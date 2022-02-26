bffc8f24374c338ce47792083976714e5ebd5a84
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-02-25 21:28:03 -0500

    binarychar.c

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main() {
+  int sum = 0;
+  char bString[9];
+
+  printf("Enter 8 bits: ");
+  scanf(" %s", bString);
+  
+  for (int i = 0; i < 8; i++) {
+    if (bString[i] == '1') {
+      int deci = 1;
+      int pwr = 7 - i;
+      for (int j = 0; j < pwr; j++) {
+        deci = deci * 2;
+      }
+      sum += deci;
+    }
+  }
+  char ascii = sum;
+  printf("Your character is: %c\n", ascii);
   return 0;
 }
