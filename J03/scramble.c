dd6ac00d8795df12aa1d15a2c7054f932afe7a4c
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-03-24 15:11:14 -0400

    J03 Correction

--- J03/scramble.c
+++ J03/scramble.c
@@ -1,3 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
 int main() {
+  char phrase[100];
+  
+  printf("Please enter a phrase: ");
+  fgets(phrase, 100, stdin);
+
+  int len = strlen(phrase); // phrase length
+  for (int i = 0; i < len; i++) {
+    unsigned char ch = phrase[i];
+    unsigned char second = 0x02;
+    unsigned char third = 0x04;
+
+    unsigned char secondch = ch & second;
+    unsigned char thirdch = ch & third;
+
+    if ((secondch >> 1) != (thirdch >> 2)) {
+      // sec = (ch & second) ^ 1;
+      // thir = (ch & third) ^ 1;
+      secondch = secondch << 1;
+      if (secondch != thirdch) {
+        ch = ch ^ 0x04;
+        ch = ch ^ 0x02;
+      }
+    }
+    phrase[i] = ch;
+  }
+  phrase[len] = '\0';
+
+  printf("scramble: %s\n", phrase);
   return 0;
 }
