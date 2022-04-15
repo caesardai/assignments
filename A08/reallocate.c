edc2f26e34eefb69a91955456be36abb1734782a
Xufeng Dai <xdai@goldengate.cs.brynmawr.edu>
2022-04-14 22:40:00 -0400

    fixed printed statement

--- A08/reallocate.c
+++ A08/reallocate.c
@@ -6,8 +6,9 @@
 
 int main (int argc, char* argv[]) {
   void *init = sbrk(0);
+  printf("The initial top of the heap is %p.\n", init);
   for (int i = 0; i < 10; i++) {
-    void *test_allocate = malloc(sizeof(char) * 10);
+    void *test_allocate = malloc(sizeof(char) * 100);
     if  (test_allocate == NULL) {
       printf("Memory allocation failed. Exiting.");
       exit(1);
@@ -17,9 +18,9 @@ int main (int argc, char* argv[]) {
   }
 
   void *current = sbrk(0);
-  int heap_size = (int)(current - init);
-  printf("The initial top of the heap is %p.\n", init);
   printf("The current top of the heap is %p.\n", current);
-  printf("We have allocated %d bytes.\n", heap_size);
+  
+  int heap_size = (int)(current - init);
+  printf("Incresed by %d (0x%d) bytes.\n", heap_size, heap_size);
   return 0 ;
 }
